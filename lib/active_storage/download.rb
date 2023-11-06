430742936a9f515d2fe455d913e4aefe4924d104
Mike Poage <mpoage@codeclimate.com>
2023-11-06 16:04:53 -0500

    foobar

--- lib/active_storage/download.rb
+++ lib/active_storage/download.rb
@@ -3,6 +3,7 @@ class ActiveStorage::Download
   # Downloading .ai as application/postscript files in Safari appends .ps to the extension.
   # Sending HTML, SVG, XML and SWF files as binary closes XSS vulnerabilities.
   # Sending JS files as binary avoids InvalidCrossOriginRequest without compromising security.
+
   CONTENT_TYPES_TO_RENDER_AS_BINARY = %w(
     text/html
     text/javascript
