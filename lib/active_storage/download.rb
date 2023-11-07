a7407593ddef0f5e48e4e399553cb2e74c3cfffc
Mike Poage <mpoage@codeclimate.com>
2023-11-07 08:05:40 -0500

    This param is actually useful

--- lib/active_storage/download.rb
+++ lib/active_storage/download.rb
@@ -44,7 +44,7 @@ class ActiveStorage::Download
       end
     end
 
-    def content_disposition(force_attachment = false),
+    def content_disposition(force_attachment = false, override_type = nil)),
       if force_attachment || content_type == BINARY_CONTENT_TYPE
         "attachment; #{escaped_filename}"
       else
